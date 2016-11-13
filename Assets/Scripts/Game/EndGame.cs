65588bcce6244f535ee636d834a2daa0e8fb88d4
Matthieu CROUZET <Matthieu CROUZET>
2016-11-13 17:11:04 -0500

    show cost

---
+++ Assets/Scripts/Game/EndGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndGame : MonoBehaviour {
+
+	public void OnDestroy(){
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
+
---
+++ Assets/Scripts/Game/EnemyUnitBuilder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class EnemyUnitBuilder : MonoBehaviour {
+	public GameObject playerBaseBuilding;
+	public GameObject buildFactory;
+	public GameObject toBuild;
+
+	void Start(){
+		InvokeRepeating("build", 10.0f, 120.0f);
+	}
+
+	public void build (){
+		UnitProperties properties = toBuild.GetComponent<UnitProperties> ();
+		BuildingProperties factoryProperties = buildFactory.GetComponent<BuildingProperties> ();
+		toBuild.SetActive (true);
+		GameObject newUnit = Instantiate (toBuild, GameObject.Find ("EnemyUnits").transform) as GameObject;
+		newUnit.transform.position = new Vector3 (buildFactory.transform.position.x-5,1.4f, buildFactory.transform.position.z);
+		newUnit.transform.name = toBuild.name;
+		EnemyUnitController euc = newUnit.AddComponent<EnemyUnitController> ();
+		euc.baseBuilding = playerBaseBuilding;
+	}
+
+}
